08dbedeac558c58c9e58ce6842b24f20d9dcc97e
FrancescoBonicelli <cescobonic@gmail.com>
2018-04-03 17:51:06 +0200

    correction of SPI test

--- useful_stuff/spi/spi.ino
+++ useful_stuff/spi/spi.ino
@@ -1,17 +1,12 @@
-// what to do with incoming data
-
 void setup (void)
 {
-
-  // have to send on master in, *slave out*
   pinMode(MISO, OUTPUT);
 
-  // turn on SPI in slave mode
+  // turn on SPI interrupt
   cli();
   SPCR = 0b11000000;
   sei();
-
-}  // end of setup
+}
 
 
 // SPI interrupt routine
@@ -20,11 +15,11 @@ ISR (SPI_STC_vect)
   byte c = SPDR;
 
   SPDR = c + 1;  // add 1
-}  // end of interrupt service routine (ISR) SPI_STC_vect
+}
 
 
 void loop (void)
 {
 
-}  // end of loop
+}
 
