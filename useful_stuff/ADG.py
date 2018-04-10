67206730835e9a669359039c8c63b55599444eb2
FrancescoBonicelli <cescobonic@gmail.com>
2018-04-10 19:17:15 +0200

    added ADG code for stepper resolution

---
+++ useful_stuff/ADG.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import smbus
+
+adg_addr = 0x48
+
+options = {
+           1 : [0, 0, 0],
+           2 : [1, 0, 0],
+           4 : [0, 1, 0],
+           8 : [1, 1, 0],
+           16 : [0, 0, 1],
+           32 : [1, 0, 1],
+           }
+
+def reset():
+    smbus.write_byte(adg_addr, 0x00)
+
+def setBit(v, index, x):
+  """Set the index:th bit of v to 1 if x is truthy, else to 0, and return the new value."""
+  mask = 1 << index   # Compute mask, an integer with just bit 'index' set.
+  v &= ~mask          # Clear the bit indicated by the mask (if x is False)
+  if x:
+    v |= mask         # If x was True, set the bit indicated by the mask.
+  return v 
+
+def ch(channel): #assume input is 1-8 -> 0-7
+    channel -=1
+    
+    if channel < 0:
+        channel = 0
+    elif channel > 7:
+        channel = 7
+    
+
+def writeChannel(channel, state):
+    value = smbus.read_byte(adg_addr)
+    value = setBit(value, ch(channel), state)
+    smbus.write_byte(adg_addr, value)
+
+bus = smbus.SMBus(1)
+
+reset()
+
+mode = input("Scegliere modalità (1:x, x = 1, 2, 4, 8, 16, 32)")
+writeChannel(0, options[mode][0])
+writeChannel(1, options[mode][1])
+writeChannel(2, options[mode][2])
+
+writeChannel(3, options[mode][0])
+writeChannel(4, options[mode][1])
+writeChannel(5, options[mode][2])
