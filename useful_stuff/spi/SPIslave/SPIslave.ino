b4cabc598367fb81efdc81a8e4af692785ef5c0c
VittorioCentorrino <34453163+VittorioCentorrino@users.noreply.github.com>
2018-04-17 18:45:53 +0100

    Modified SPI code

--- useful_stuff/spi/SPIslave/SPIslave.ino
+++ useful_stuff/spi/SPIslave/SPIslave.ino
@@ -8,11 +8,12 @@ void setup (void)
   sei();
 }
 
-volatile char buf [4];
-volatile char ver [3];
-volatile byte pos ref1;
-volatile float y x rz;
-volatile bool trigger2 trigger pinkie cmdstart cmdstop;
+volatile char buf[4];
+volatile char ver[3];
+volatile byte pos, ref1;
+volatile float y, x, rz;
+volatile bool trigger2, trigger, pinkie, cmdstart, cmdstop;
+volatile float Roll,Pitch,Pressure,Temperature;
 
 
 // SPI interrupt routine
@@ -24,7 +25,7 @@ ISR (SPI_STC_vect)
     {
     SPDR = buf [pos];
     }
- switch pos 
+ switch (pos) 
  {
 
  case 0:
@@ -41,16 +42,19 @@ ISR (SPI_STC_vect)
 
  case 3:
  ref1 = c;
-
+ 
+ ver[0] = bitRead(c,7);
+ ver[1] = bitRead(c,6);
+ ver[2] = bitRead(c,5);
  cmdstop = bitRead(c,4);
  cmdstart = bitRead(c,3);
  pinkie = bitRead(c,2);
  trigger = bitRead(c,1);
  trigger2 = bitRead(c,0);
  
- if (ver[0] ==1 && ver[1]=0 && ver[2]=1)
+ if (ver[0] ==1 && ver[1]==0 && ver[2]==1)
  {
-  Serial.print("Condition Verified"
+  Serial.println("Condition Verified");
  }
  break;
 
