4840679ad472ab71f0b709a421fbe3c4ea8aba83
FrancescoBonicelli <cescobonic@gmail.com>
2018-04-19 14:54:26 +0200

    braccio: added enable pin

--- useful_stuff/Stepper/Braccio.py
+++ useful_stuff/Stepper/Braccio.py
@@ -9,7 +9,8 @@ from errmess_publisher import *
 
 CALIBRATE = 6 # Joystick botton to give 'start calibration'
 DIR = 20   # Direction GPIO Pin
-STEP = 21  # Step GPIO Pin
+STEP = 16  # Step GPIO Pin
+EN_n1 = 21 # Enable driver pin
 CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
 SPR = 400   # Steps per Revolution (360 / 0.9) Sensibility of nema 17H is 0.9deg
@@ -17,7 +18,9 @@ SPR = 400   # Steps per Revolution (360 / 0.9) Sensibility of nema 17H is 0.9deg
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
+GPIO.setup(EN_n1, GPIO.OUT)
 GPIO.output(DIR, CCW) #controllare senso per inizializzazione
+GPIO.output(EN_n1, 1) # disable driver
 
 step_count = SPR/2
 delay = .01 # 4/400 seconds/steps --> I'm saing that I need 4 seconds for a complete rotation
@@ -72,6 +75,7 @@ def main():
         
     if (Status == -1):
         GPIO.output(DIR, CCW)
+        GPIO.output(EN_n1, 0)
         
         while 1:
             GPIO.output(STEP, GPIO.HIGH)
@@ -82,6 +86,7 @@ def main():
             if Sensor == 1:       # I find the initial position
                 Angle = 0
                 Error = 0
+                GPIO.output(EN_n1, 1)
                 break
 
 # Waiting command from Joystick 
@@ -93,8 +98,10 @@ def main():
         else:
             Angle_new = 180*((Status/2)+0.5)            # --> Joy rage [-1,+1] ; Angle range [0,180]
                                                         # Joy=-1 --> angle=0 i.e. nitial position
-            Actual_status = Status                      
+            Actual_status = Status
+            GPIO.output(EN_n1, 0)
             NormMode(Angle , Angle_new)
+            GPIO.output(EN_n1, 1)
         
             Angle = Angle_new
         
