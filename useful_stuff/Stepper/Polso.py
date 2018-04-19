5165243587d233662650b6fae53d98d96151f1cc
FrancescoBonicelli <cescobonic@gmail.com>
2018-04-19 14:33:44 +0200

    correstion of "polso" (divided axis and buttons nodes)

--- useful_stuff/Stepper/Polso.py
+++ useful_stuff/Stepper/Polso.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  7 23:08:07 2018
-
-@author: DETJON
-"""
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
@@ -11,7 +5,6 @@ import rospy
 from std_msgs.msg import String
 from politocean.msg import *
 from time import sleep
-import timeit
 from errmess_publisher import *
 
 DIR = 27   # Direction GPIO Pin
@@ -28,58 +21,44 @@ GPIO.setup(STEP, GPIO.OUT)
 GPIO.setup(EN_n1, GPIO.OUT)
 GPIO.output(EN_n1, 1) # En attivo basso
 
-delay = .01
+delay = 0.0005
 
 def Rotate_CW():
-    global e_butt
     global ry
     
     GPIO.output(DIR, CW)
-    
-  #  while ( e_butt == 1 ):
-        
-        if (ry > 0):
             
-            delay_new = delay/(ry*50)
-            GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay_new)
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(delay_new)
-            
- #       else:
- #           return 
+    if (ry > 0):
+        delay_new = delay/ry
+        GPIO.output(STEP, GPIO.HIGH)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay_new)
         
 def Rotate_CCW():
-    global e_butt
     global ry
     
     GPIO.output(DIR, CCW)
-    
- #   while ( e_butt == 1 ):
-        
-        if (ry < 0):
             
-            delay_new = -delay/(ry*50)
-            GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay_new)
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(delay_new)
-            
-#        else: 
-#            break 
+    if (ry < 0):
+        delay_new = -delay/ry
+        GPIO.output(STEP, GPIO.HIGH)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay_new)
         
 def joystickButtCallback(data):
-    
     global e_butt
-    global ry
     
     if data.ID == "e_butt":
         e_butt = data.status
+    
+
+def joystickAxisCallback(data):
+    global ry
+    
     if data.ID == "ry":
         ry = data.status
         
 def main():
-    
     global e_butt
     global ry
     
@@ -90,20 +69,22 @@ def main():
     rospy.init_node("Polso", anonymous=False)
     #subscriber
     joystick_butt_sub = rospy.Subscriber("joystick_buttons", joystick_buttons, joystickButtCallback)
+    joystick_axis_sub = rospy.Subscriber("joystick_axis", joystick_axis, joystickAxisCallback)
+
     
     errMessInit() #init topics
     
     while not rospy.is_shutdown(): 
     
         if ( e_butt == 1 ) :
-            GPIO.output(EN_n1,0)
+            GPIO.output(EN_n1, 0)
             if (ry > 0):
                 Rotate_CW()
             else: 
                 Rotate_CCW()
         else :
-            GPIO.output(EN_n1,1)
-            sleep(1)
+            GPIO.output(EN_n1, 1)
+            sleep(0.1)
             
 if __name__ == '__main__':
     main()
