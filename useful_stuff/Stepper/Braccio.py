62bd3d1de1bc75e6f58ee81208767dc0d98baea0
Detjon <34067851+Detjon13@users.noreply.github.com>
2018-04-26 11:22:32 +0200

    Added ADG in Braccio

--- useful_stuff/Stepper/Braccio.py
+++ useful_stuff/Stepper/Braccio.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  3 18:52:44 2018
-
-@author: DETJON
-"""
 #!/usr/bin/env python
 
 from time import sleep
@@ -11,13 +5,13 @@ import RPi.GPIO as GPIO
 import rospy
 from std_msgs.msg import String
 from politocean.msg import *
-import timeit
 from errmess_publisher import *
-
+from ADG import micro_stepping
 
 CALIBRATE = 6 # Joystick botton to give 'start calibration'
 DIR = 20   # Direction GPIO Pin
-STEP = 21  # Step GPIO Pin
+STEP = 16  # Step GPIO Pin
+EN_n1 = 21 # Enable driver pin
 CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
 SPR = 400   # Steps per Revolution (360 / 0.9) Sensibility of nema 17H is 0.9deg
@@ -25,116 +19,92 @@ SPR = 400   # Steps per Revolution (360 / 0.9) Sensibility of nema 17H is 0.9deg
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
+GPIO.setup(EN_n1, GPIO.OUT)
 GPIO.output(DIR, CCW) #controllare senso per inizializzazione
+GPIO.output(EN_n1, 1) # disable driver
 
 step_count = SPR/2
-delay = .01 # 4/400 seconds/steps --> I'm saing that I need 4 seconds for a complete rotation
-            # --> 2sec for 180deg and so on
+delay = .001
 
-
-
-def joystickButtCallback(data):
+def joystickAxisCallback(data):
     global Status
+    global Angle_new
     
     if data.ID == "z":
         Status = data.status
-        
-        
-        
-def NormMode(Old , New):
-    global Actual_status
-    global Status
-        
-    if New > Old:
-        GPIO.output(DIR, CW)                                  
-        for i in range((New-Old)/(0.9)):
-            GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay)
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(delay)
-            if Actual_status != Status:  
-                break
+    
+        Angle_new = 180*((Status/2)+0.5)       # --> Joy rage [-1,+1] ; Angle range [0,180]
+                                               # Joy=-1 --> angle=0 i.e. nitial position
 
-    elif New < Old:
-        GPIO.output(DIR, CCW)                                  
-        for i in range((Old-New)/(0.9)):
+def NormMode():
+    global Status
+    global Angle
+    global Angle_new
+        
+    if Angle_new-0.45 > Angle:
+        GPIO.output(DIR, CW)                                  
+        while Angle < Angle_new:
             GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay)
             GPIO.output(STEP, GPIO.LOW)
+            Angle += 0.9
             sleep(delay)
-            if Actual_status != Status:  
-                break
+
+    elif Angle_new < Angle:
+        GPIO.output(DIR, CCW)                                  
+        while Angle > Angle_new+0.45:
+            GPIO.output(STEP, GPIO.HIGH)
+            GPIO.output(STEP, GPIO.LOW)
+            Angle -= 0.9
+            sleep(delay)
                     
 def main():
 ## CALIBRATION
     global Status
-    global Actual_status
+    global Angle
+    
+    Status = 0
+    Actual_status = 0
     Error = 1
     
     # set node name
     rospy.init_node("Braccio", anonymous=False)
     #subscriber
-    joystick_butt_sub = rospy.Subscriber("joystick_axis", joystick_axis, joystickButtCallback)
+    joystick_axis_sub = rospy.Subscriber("joystick_axis", joystick_axis, joystickAxisCallback)
     
     errMessInit() #init topics
-    
     
     while (Status != -1):
         sleep(.1)
         
     if (Status == -1):
-        for x in range(step_count):
+        GPIO.output(DIR, CCW)
+        GPIO.output(EN_n1, 0)
+        
+        while 1:
             GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay)
             GPIO.output(STEP, GPIO.LOW)
             sleep(delay)
-            Sensor = Val_from_magnetic_sensor 
+            Sensor = 1 # value from magnetic sensor
             
             if Sensor == 1:       # I find the initial position
                 Angle = 0
                 Error = 0
+                GPIO.output(EN_n1, 1)
                 break
-                
+
 # Waiting command from Joystick 
     while Error == 0  :
-        
-            
-        if Status > 1 | Status < -1:
+        if Status > 1 or Status < -1:
             Error = 1
             publishErrors("Braccio", "Errore lettura Joystick\n")
             
         else:
-            
-            Angle_new = 180*((Status/2)+0.5)            # --> Joy rage [-1,+1] ; Angle range [0,180]
-                                                        # Joy=-1 --> angle=0 i.e. nitial position
-            Actual_status = Status                      
-            NormMode(Angle , Angle_new)
-        
-            Angle = Angle_new
-        
+            micro_stepping(0,2)
+            GPIO.output(EN_n1, 0)
+            NormMode()
+            GPIO.output(EN_n1, 1)
+
 if __name__ == '__main__':
     main()
         
 GPIO.cleanup()
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
