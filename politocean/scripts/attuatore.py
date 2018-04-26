9880bf406d26623d220677ab1b80c23824d1f47e
Unknown <s251727@studenti.polito.it>
2018-04-26 20:02:42 +0200

    update attuatore.py

--- politocean/scripts/attuatore.py
+++ politocean/scripts/attuatore.py
@@ -9,29 +9,37 @@ from errmess_publisher import *
 #limitare la corrente a 100 mA
 #chiedere agli elettronici dove leggere la corrente 
 
-global pwm_pin=24
-global dir_pin=23
+global pwm_pin
+global dir_pin
+global fast
+fast=0
+pwm_pin=24
+dir_pin=23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pwm_pin, GPIO.OUT)
 GPIO.setup(dir_pin, GPIO.OUT)
 my_pwm=GPIO.PWM(pwm_pin,100)
 my_pwm.start(0)
 
-def apri(d_butt)
-	global d_butt
-	global fast
-	GPIO.output(dir_pin, 0) #0 indica la direzione dell'attuatore
-        my_pwm.ChangeDutyCycle(fast)
-	#leggi il val di rx imposta la corrente e apri
-	
+def apri():
+        global d_butt
+        global fast
+        while(d_butt):
+                GPIO.output(dir_pin, 1) #0 indica la direzione dell'attuatore
+                my_pwm.ChangeDutyCycle(fast)
+        my_pwm.stop()
+        #leggi il val di rx imposta la corrente e apri
 
-def chiudi (i_butt)
-	global i_butt
-	global fast
-	GPIO.output(dir_pin, 1) #1 indica la direzione dell'attuatore
-	my_pwm.ChangeDutyCycle(fast)
-	#leggi il val di rx imposta la corrente e chiudi 
-	
+
+def chiudi ():
+        global i_butt
+        global fast
+        while(i_butt):
+                GPIO.output(dir_pin, 0) #1 indica la direzione dell'attuatore
+		my_pwm.ChangeDutyCycle(fast)
+        my_pwm.stop()
+        #leggi il val di rx imposta la corrente e chiudi 
+
 
 
 
@@ -41,43 +49,48 @@ def joystickButtCallback(data):
 
     if data.ID == "i_butt":
         i_butt = data.status
-    elif data.ID == "d_butt":
-	d_butt=data.status 
-	
+    if data.ID == "d_butt":
+        d_butt=data.status
+
 def joystickAxisCallback(data):
     global rx
     global fast
-    
+
     if data.ID == "rx":
         rx = data.status
-	fast=(rx+1)*40+20
-        
+        fast=(rx+1)*40+20
+
 def main():
-    
+    global i_butt
+    global d_butt
+    global rx
+
     # set node name
     rospy.init_node("Attuatore", anonymous=False)
     #subscriber
     joystick_butt_sub = rospy.Subscriber("joystick_buttons", joystick_buttons, joystickButtCallback)
     joystick_axis_sub = rospy.Subscriber("joystick_axis", joystick_axis, joystickAxisCallback)
-    ry=0
+    rx=0
     i_butt=0
     d_butt=0
 
     errMessInit() #init topics
-    
-    while not rospy.is_shutdown(): 
 
-	if ( d_butt == 1 ) :
-           apri(d_butt)
-	
-	if ( i_butt == 1) :
-	   chiudi(i_butt)
+ while not rospy.is_shutdown():
 
-            
+        if ( d_butt == 1 ) :
+           apri()
+
+        if ( i_butt == 1) :
+           chiudi()
+
+
 if __name__ == '__main__':
     main()
-    
+
 GPIO.cleanup()
-    
-    
-    
+
+
+
+	
+
