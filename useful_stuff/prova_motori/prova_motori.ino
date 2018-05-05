6894e5309adcefdbd58525ba2dda5ab56f0222f5
FrancescoBonicelli <cescobonic@gmail.com>
2018-05-05 10:42:27 +0200

    added new motors

--- useful_stuff/prova_motori/prova_motori.ino
+++ useful_stuff/prova_motori/prova_motori.ino
@@ -1,27 +1,27 @@
- #include <Servo.h>
+#include <Servo.h>
 
-#define RE_n_enable 3
-
-byte escPin_1 = 10;
+byte escPin_1 = 2;
 Servo T200_1;
-byte escPin_2 = 9;
+byte escPin_2 = 3;
 Servo T200_2;
-byte escPin_3 = 8;
+byte escPin_3 = 4;
 Servo T200_3;
-byte escPin_4 = 7;
+byte escPin_4 = 5;
 Servo T200_4;
 byte escPin_5 = 6;
 Servo T200_5;
-byte escPin_6 = 5;
+byte escPin_6 = 7;
 Servo T200_6;
+byte escPin_7 = 8;
+Servo T200_7;
+byte escPin_8 = 9;
+Servo T200_8;
 
-int test, indice=1;
+int indice=1;
 
 void setup()
 {
   Serial.begin(9600);
-
-  pinMode(RE_n_enable , OUTPUT);
   
   T200_1.attach(escPin_1);
   T200_2.attach(escPin_2);
@@ -29,6 +29,8 @@ void setup()
   T200_4.attach(escPin_4);
   T200_5.attach(escPin_5);
   T200_6.attach(escPin_6);
+  T200_7.attach(escPin_7);
+  T200_8.attach(escPin_8);
  
   T200_1.writeMicroseconds(1500); // send "stop" signal to ESC.
   T200_2.writeMicroseconds(1500);
@@ -36,50 +38,46 @@ void setup()
   T200_4.writeMicroseconds(1500);
   T200_5.writeMicroseconds(1500);
   T200_6.writeMicroseconds(1500);
+  T200_7.writeMicroseconds(1500);
+  T200_8.writeMicroseconds(1500);
   
   delay(1000);
-  
-  digitalWrite(RE_n_enable, LOW);
 }
 
 void loop() {
+
   while(!Serial.available());
-  
-  indice=Serial.read();
+  indice=Serial.read()-'0';
   
   switch(indice) {
-    case '1':
-      T200_1.writeMicroseconds(1600);
-      delay(2000);
+    case 1:
+      T200_1.writeMicroseconds(1550);
       break;
-    case '2':
-      T200_2.writeMicroseconds(1600);
-      delay(2000);
+    case 2:
+      T200_2.writeMicroseconds(1550);
       break;
-    case '3':
-      T200_3.writeMicroseconds(1600);
-      delay(2000);
+    case 3:
+      T200_3.writeMicroseconds(1550);
       break;
-    case '4':
-      T200_4.writeMicroseconds(1600);
-      delay(2000);
+    case 4:
+      T200_4.writeMicroseconds(1550);
       break;
-    case '5':
-      T200_5.writeMicroseconds(1600);
-      delay(2000);
+      case 5:
+      T200_5.writeMicroseconds(1550);
       break;
-    case '6':
-      T200_6.writeMicroseconds(1600);
-      delay(2000);
+    case 6:
+      T200_6.writeMicroseconds(1550);
       break;
-    case 0:
+    case 7:
+      T200_7.writeMicroseconds(1550);
+      break;
+    case 8:
+      T200_8.writeMicroseconds(1550);
       break;
     default:
-      digitalWrite(RE_n_enable, HIGH);
-      Serial.println((int)indice);
-      delay(50);
-      digitalWrite(RE_n_enable, LOW);
+      Serial.println("Error indexing motors");
   }
+  delay(5000);
   
   T200_1.writeMicroseconds(1500);
   T200_2.writeMicroseconds(1500);
@@ -87,7 +85,9 @@ void loop() {
   T200_4.writeMicroseconds(1500);
   T200_5.writeMicroseconds(1500);
   T200_6.writeMicroseconds(1500);
- 
+  T200_7.writeMicroseconds(1500);
+  T200_8.writeMicroseconds(1500);
+
 }
 
 
