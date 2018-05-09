2141d9d73582011c3c8eec7b14cbc37463d2e328
FrancescoBonicelli <cescobonic@gmail.com>
2018-05-09 15:28:45 +0200

    new arm node

--- politocean/scripts/braccio.py
+++ politocean/scripts/braccio.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 from time import sleep
 import RPi.GPIO as GPIO
 import rospy
@@ -8,104 +7,71 @@ from politocean.msg import *
 from errmess_publisher import *
 from ADG import micro_stepping
 
-CALIBRATE = 6 # Joystick botton to give 'start calibration'
-DIR = 20   # Direction GPIO Pin
-STEP = 16  # Step GPIO Pin
-EN_n1 = 21 # Enable driver pin
+DIR1 = 20   # Direction GPIO Pin
+STEP1 = 16  # Step GPIO Pin
+EN_n1 = 21  # Enable Pin active low
+
 CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
-SPR = 400   # Steps per Revolution (360 / 0.9) Sensibility of nema 17H is 0.9deg
+
+delay = 0.001
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
+
+GPIO.setup(DIR1, GPIO.OUT)
+GPIO.setup(STEP1, GPIO.OUT)
 GPIO.setup(EN_n1, GPIO.OUT)
-GPIO.output(DIR, CCW) #controllare senso per inizializzazione
-GPIO.output(EN_n1, 1) # disable driver
+GPIO.output(DIR1, CCW)
+GPIO.output(EN_n1, 1)
 
-step_count = SPR/2
-delay = .001
 
-def joystickAxisCallback(data):
-    global Status
-    global Angle_new
-    
-    if data.ID == "z":
-        Status = data.status
-    
-        Angle_new = 180*((Status/2)+0.5)       # --> Joy rage [-1,+1] ; Angle range [0,180]
-                                               # Joy=-1 --> angle=0 i.e. nitial position
+def joystickButtCallback(data):
+    global Status3
+    global Status4
 
-def NormMode():
-    global Status
-    global Angle
-    global Angle_new
-        
-    if Angle_new-0.45 > Angle:
-        GPIO.output(DIR, CW)                                  
-        while Angle < Angle_new:
-            GPIO.output(STEP, GPIO.HIGH)
-            GPIO.output(STEP, GPIO.LOW)
-            Angle += 0.9
-            sleep(delay)
+    if data.ID == "back_up":
+        Status3 = data.status
+    if data.ID == "back_down":
+        Status4 = data.status
 
-    elif Angle_new < Angle:
-        GPIO.output(DIR, CCW)                                  
-        while Angle > Angle_new+0.45:
-            GPIO.output(STEP, GPIO.HIGH)
-            GPIO.output(STEP, GPIO.LOW)
-            Angle -= 0.9
-            sleep(delay)
-                    
 def main():
-## CALIBRATION
-    global Status
-    global Angle
-    
-    Status = 0
-    Actual_status = 0
-    Error = 1
-    
+    global Status3
+    global Status4
+    Status3 = 0
+    Status4 = 0
+
     # set node name
     rospy.init_node("Braccio", anonymous=False)
     #subscriber
-    joystick_axis_sub = rospy.Subscriber("joystick_axis", joystick_axis, joystickAxisCallback)
-    
+    joystick_butt_sub = rospy.Subscriber("joystick_buttons", joystick_buttons, joystickButtCallback)
     errMessInit() #init topics
+    micro_stepping(1,2)
 
-    micro_stepping(0,2)
-    
-    while (Status != -1):
-        sleep(.1)
-        
-    if (Status == -1):
-        GPIO.output(DIR, CCW)
-        GPIO.output(EN_n1, 0)
-        
-        while 1:
-            GPIO.output(STEP, GPIO.HIGH)
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(delay)
-            Sensor = 1 # value from magnetic sensor
-            
-            if Sensor == 1:       # I find the initial position
-                Angle = 0
-                Error = 0
-                GPIO.output(EN_n1, 1)
-                break
+    while not rospy.is_shutdown(): 
+        while (Status3):
+		GPIO.output(EN_n1, 0)
+	 	GPIO.output(DIR1, CW)		#CV-->su
 
-# Waiting command from Joystick 
-    while Error == 0  :
-        if Status > 1 or Status < -1:
-            Error = 1
-            publishErrors("Braccio", "Errore lettura Joystick\n")
-            
-        else:
-            GPIO.output(EN_n1, 0)
-            NormMode()
-            GPIO.output(EN_n1, 1)
+	 	GPIO.output(STEP1, GPIO.HIGH)
+    	 	sleep(delay)
+    	 	GPIO.output(STEP1, GPIO.LOW)
+    	 	sleep(delay)
+
+        GPIO.output(EN_n1, 1)
+
+      	while (Status4):
+		GPIO.output(EN_n1, 0)
+	 	GPIO.output(DIR1, CCW)		#CVV-->giu
+
+    	 	GPIO.output(STEP1, GPIO.HIGH)
+    	 	sleep(delay)
+    	 	GPIO.output(STEP1, GPIO.LOW)
+    	 	sleep(delay)
+
+        GPIO.output(EN_n1,1)
+        sleep(0.1)
 
 if __name__ == '__main__':
     main()
-        
+    
 GPIO.cleanup()
