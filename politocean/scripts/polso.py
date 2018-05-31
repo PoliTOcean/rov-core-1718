9e6b3fe15cf3cb2b33a03a3979e92d64f8606639
Unknown <s251727@studenti.polito.it>
2018-05-31 19:14:46 +0200

    modified polso.py

--- politocean/scripts/polso.py
+++ politocean/scripts/polso.py
@@ -14,7 +14,9 @@ CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
 SPR = 200  # Steps per Revolution (360 /1.7) Sensibility of nema 1.7deg
 EN_n1 = 22
+cont = False
 
+ 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
@@ -47,24 +49,33 @@ def Rotate_CCW():
         
 def joystickButtCallback(data):
     global e_butt
-    
+    global c_butt
+    global cont
+
     if data.ID == "e_butt":
         e_butt = data.status
-    
+    if data.ID == "c_butt" and data.status == True :
+	cont = not cont #cambio il valore ogni volta che il pulsante trigghera verso l'alto
+	
 
 def joystickAxisCallback(data):
     global ry
     
+
     if data.ID == "ry":
         ry = data.status
+    
         
 def main():
     global e_butt
     global ry
-    
+    global cont
+    global c_butt
+
     e_butt = 0
     ry = 0
-    
+    cont = False
+
     # set node name
     rospy.init_node("Polso", anonymous=False)
     #subscriber
@@ -75,7 +86,7 @@ def main():
     errMessInit() #init topics
     
     while not rospy.is_shutdown(): 
-    
+       
         if ( e_butt == 1 ) :
             micro_stepping(1,1)
             GPIO.output(EN_n1, 0)
@@ -83,10 +94,10 @@ def main():
                 Rotate_CW()
             else: 
                 Rotate_CCW()
-        else :
-            GPIO.output(EN_n1, 1)
+        else:
+            GPIO.output(EN_n1, not cont)
             sleep(0.1)
-            
+
 if __name__ == '__main__':
     main()
     
